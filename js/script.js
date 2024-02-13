758d149e36b274f2e724dcd21ed957231a29c710
post-coder <gabrielspanu.dev@gmail.com>
2024-02-13 12:43:30 +0100

    oggetti con array all'interno

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 
-const classi = [
-    ["Gian Marco", "Roberto", "Alan", "Francesco"],
-    ["Luca", "Ignazio", "Esmeraldo"],
-    ["Igor", "Emanuele", "Tottore"]
-];
+const classe = {
+    nome: "Classe 118",
+    partenza: "29 Nov 2023",
+    alunni: ["Giorgio", "Luca", "Miriam", "Marco"],
+    professori: ['Elisabetta', 'Gabriel', 'Daniele', 'Davide'],
+}
 
-// per stampare il primo elemento della seconda riga:
-console.log(classi[1][0]);
-
-
-
-console.log("------------");
-
-// per stampare tutti gli elementi:
-for(let i = 0; i < classi.length; i++) {
+// primo alunno della classe
+console.log(classe.alunni[0])
 
 
-    const arrayAttuale = classi[i]; // array
+console.log("------------------")
 
-    for(let j = 0; j < arrayAttuale.length; j++) {
-        console.log( arrayAttuale[j] )
-    }
+// stampiamo il quarto professore inserito nella lista
+console.log(classe.professori[3])
+
+console.log("------------------")
+
+// stampiamo tutti i professori del mio oggetto
+for (let i = 0; i < classe.professori.length; i++) {
+
+    console.log(classe.professori[i])
 
 }
